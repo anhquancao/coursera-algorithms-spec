3950c229c216890db591e3bdae3a7650b7047cd8
Cao Anh Quan <aquancva@gmail.com>
2017-01-01 23:38:45 +0700

    2 SUM problem use hash map

---
+++ src/main/scala/TwoSum.scala
@@ -0,0 +1,31 @@
+import scala.io.Source
+
+/**
+  * Created by caoquan on 11/21/16.
+  */
+object TwoSum {
+
+
+  def main(args: Array[String]): Unit = {
+
+    // Read data from the file
+    val filename = "/Users/caoquan/Desktop/data.txt"
+    // put data into a hash table
+    var maps: Map[Long, Long] = Map()
+    var count = 0
+    for (line <- Source.fromFile(filename).getLines()) {
+      val elem = line.toLong
+      maps += (elem -> elem)
+    }
+    for (t <- -10000 to 10000) {
+      maps.values.foreach {
+        v => {
+          if (maps.contains(t - v)) {
+            count += 1
+          }
+        }
+      }
+    }
+    println("count: " + count)
+  }
+}

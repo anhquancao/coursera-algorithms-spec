ca7b3d2c9633bd9e42f97172bf7d04006a2228a0
Cao Anh Quan <aquancva@gmail.com>
2017-01-08 07:19:55 +0100

    twosum

--- src/main/scala/TwoSum.scala
+++ src/main/scala/TwoSum.scala
@@ -9,6 +9,7 @@ object TwoSum {
   def main(args: Array[String]): Unit = {
 
     // Read data from the file
+    println("Input: ")
     val filename = "/Users/caoquan/Desktop/data.txt"
     // put data into a hash table
     var maps: Map[Long, Long] = Map()
@@ -18,6 +19,7 @@ object TwoSum {
       maps += (elem -> elem)
     }
     for (t <- -10000 to 10000) {
+      println("value: " + t)
       maps.values.foreach {
         v => {
           if (maps.contains(t - v)) {
